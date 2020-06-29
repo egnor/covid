70f1f1a82127ae99fdbc8c9075eb181f44133826
egnor <egnor@ofb.net>
2020-06-29 09:27:46 -0700

    HTML labels

--- plot_trends.py
+++ plot_trends.py
@@ -21,6 +21,7 @@ import fetch_cdc_mortality
 import fetch_census_population
 import fetch_covid_tracking
 
+MetricData = collections.namedtuple('MetricData', 'name color frame')
 
 RegionData = collections.namedtuple('RegionData', 'name population metrics')
 
@@ -70,15 +71,11 @@ for fips, covid in covid_states.groupby(by='fips'):
         frame = pandas.DataFrame(dict(
             date=date, raw=per_cap, value=per_cap.rolling(7).mean()))
         frame.set_index('date', inplace=True)
-        frame.name = name_with_capita(name, capita)
-        frame.color = color
-        return frame
+        return MetricData(name_with_capita(name, capita), color, frame)
 
     def threshold(name, color, v, capita):
         frame = pandas.DataFrame(dict(value=[v * capita / census.POP]))
-        frame.name = name_with_capita(name, capita)
-        frame.color = color
-        return frame
+        return MetricData(name_with_capita(name, capita), color, frame)
 
     cov = covid.sort_values(by='date')
     d = cov.date
@@ -102,29 +99,27 @@ print('Making plots...')
 os.makedirs(args.output_dir, exist_ok=True)
 
 min_date = pandas.to_datetime('2020-03-01')
-max_date = max(
-    m.index.max() for r in regions for m in r.metrics
-    if m.index.name == 'date') + pandas.to_timedelta(1, unit='days')
+max_date = pandas.to_timedelta(1, unit='days') + max(
+    m.frame.index.max() for r in regions for m in r.metrics
+    if pandas.api.types.is_datetime64_any_dtype(m.frame.index))
 
 plots = []
 for region in regions:
     print(f'Plotting {region.name}...')
     figure = matplotlib.figure.Figure(figsize=(8, 8))
-    figure.add_artist(matplotlib.text.Text(
-        0.5, 0.5, region.name,
-        ha='center', va='center', wrap=True,
-        fontsize=65, fontweight='bold', alpha=0.25))
 
     axes = figure.add_subplot()
     for i, m in enumerate(region.metrics):
-        if m.index.name == 'date':
-            if 'raw' in m.columns and m.raw.any():
-                axes.plot(m.index, m.raw, color=m.color, alpha=0.5, lw=1)
-            if 'value' in m.columns and m.value.any():
-                axes.plot(m.index, m.value, color=m.color, label=m.name, lw=2)
+        if pandas.api.types.is_datetime64_any_dtype(m.frame.index):
+            if 'raw' in m.frame.columns and m.frame.raw.any():
+                axes.plot(m.frame.index, m.frame.raw,
+                          color=m.color, alpha=0.5, lw=1)
+            if 'value' in m.frame.columns and m.frame.value.any():
+                axes.plot(m.frame.index, m.frame.value,
+                          color=m.color, label=m.name, lw=2)
         else:
             axes.hlines(
-                m.value, xmin=min_date, xmax=max_date,
+                m.frame.value, xmin=min_date, xmax=max_date,
                 label=m.name, color=m.color, linestyle='--', alpha=0.5)
 
     axes.grid(color='g', alpha=0.2)
@@ -146,25 +141,48 @@ for region in regions:
         thumb_url=f'{urllib.parse.quote(file_base)}_thumb.png')
     plots.append(plot)
 
+    # Thumbnail version
     axes.xaxis.set_major_formatter(matplotlib.ticker.NullFormatter())
     axes.yaxis.set_major_formatter(matplotlib.ticker.NullFormatter())
     figure.set_tight_layout(True)
     figure.savefig(f'{args.output_dir}/{file_base}_thumb.png', dpi=50)
 
+    # Full version
     axes.legend(loc='upper left')
     month_formatter = matplotlib.dates.ConciseDateFormatter(month_locator)
     axes.xaxis.set_major_formatter(month_formatter)
     axes.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
+
+    # TODO - use Roboto instead of default font for consistency
+    figure.add_artist(matplotlib.text.Text(
+        0.5, 0.5, region.name,
+        ha='center', va='center', wrap=True,
+        fontsize=65, fontweight='bold', alpha=0.25))
     figure.savefig(f'{args.output_dir}/{file_base}_full.png', dpi=200)
+
+stylesheet_url = 'style.css'
+open(f'{args.output_dir}/{stylesheet_url}', 'w').write('''
+body {font-family: 'Roboto', sans;}
+.thumb {display: inline-block; position: relative;}
+.thumb .thumb_label {
+    position: absolute; top: 18px; width: 100%; text-align: center;
+    font-size: 24px; font-weight: 900;
+    color: black; opacity: 0.5;
+}
+''')
 
 doc, tag, text, line = yattag.Doc().ttl()
 with tag('html'):
     with tag('head'):
         line('title', f'COVID-19 trends ({max_date.strftime("%Y-%m-%d")})')
+        doc.stag('link', rel='stylesheet', href=stylesheet_url)
+        doc.stag('link', rel='stylesheet',
+                 href='https://fonts.googleapis.com/css?family=Roboto')
     with tag('body'):
         line('h1', f'COVID-19 trends ({max_date.strftime("%Y-%m-%d")})')
         for plot in plots:
-            with tag('a', href=plot.image_url):
+            with tag('a', klass='thumb', href=plot.image_url):
+                doc.line('span', plot.region.name, klass='thumb_label')
                 doc.stag('img', width=200, height=200, src=plot.thumb_url)
 
 open(f'{args.output_dir}/index.html', 'w').write(doc.getvalue())
