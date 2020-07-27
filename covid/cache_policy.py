502734b184d0f03c0651d2851ef004eba40a4c31
Dan Egnor <egnor@ofb.net>
2020-07-27 12:43:54 -0700

    simplify caching

--- covid/cache_policy.py
+++ covid/cache_policy.py
@@ -1,48 +1,34 @@
 # Common settings and defaults for requests_cache layer.
 
 import argparse
-import calendar
 import datetime
 import pathlib
 
 import cachecontrol
 import cachecontrol.caches.file_cache
 import cachecontrol.heuristics
-import email.utils
 import requests
 
 
 # Reusable command line arguments for log fetching.
 argument_parser = argparse.ArgumentParser(add_help=False)
 argument_group = argument_parser.add_argument_group('data caching')
-argument_group.add_argument(
-    '--no_cache', default=False, action='store_true')
+argument_group.add_argument('--cache_seconds', type=int)
 argument_group.add_argument(
     '--cache_dir', type=pathlib.Path,
     default=pathlib.Path.home() / 'http_cache')
-argument_group.add_argument(
-    '--cache_time', type=datetime.timedelta,
-    default=datetime.timedelta(hours=1))
 
 
-class SimpleTimeHeuristic(cachecontrol.heuristics.BaseHeuristic):
-    def __init__(self, cache_time):
-        self._cache_time = cache_time
-
-    def update_headers(self, response):
-        date = email.utils.parsedate(response.headers['date'])
-        et = datetime.datetime(*date[:6]) + self._cache_time
-        return {
-            'expires': email.utils.formatdate(calendar.timegm(et.timetuple())),
-            'cache-control': 'public',
-        }
-
-
-def new_session(parsed_args):
+def new_session(args):
     """Returns a new CachedSession per the supplied command line args."""
-    return (requests.Session() if parsed_args.no_cache else
-            cachecontrol.CacheControl(
-                requests.Session(),
-                heuristic=SimpleTimeHeuristic(parsed_args.cache_time),
-                cache=cachecontrol.caches.file_cache.FileCache(
-                    parsed_args.cache_dir)))
+
+    session = requests.Session()
+    if args.cache_seconds:
+        adapter = cachecontrol.CacheControlAdapter(
+            cache=cachecontrol.caches.file_cache.FileCache(args.cache_dir),
+            heuristic=cachecontrol.heuristics.ExpiresAfter(
+                seconds=args.cache_seconds))
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+
+    return session
