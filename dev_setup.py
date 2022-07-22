e3fdad4d78611577d5630860733e101f991cdee8
Dan Egnor <egnor@ofb.net>
2022-07-22 10:22:44 -0700

    support various versions of libproj

--- dev_setup.py
+++ dev_setup.py
@@ -11,7 +11,7 @@ source_dir = Path(__file__).resolve().parent
 print("=== System packages (sudo apt install ...) ===")
 apt_install = [
     "python3", "python3-dev", "python3-pip", "python3-venv",
-    "libgeos-dev", "libproj-dev",
+    "libcairo-dev", "libgeos-dev", "libproj-dev",
 ]
 dpkg_query_command = ["dpkg-query", "--show", "--showformat=${Package}\\n"]
 installed = set(check_output(dpkg_query_command).decode().split())
@@ -26,16 +26,22 @@ if not venv_dir.is_dir():
     print(f"Creating {venv_dir}...")
     venv.create(venv_dir, symlinks=True, with_pip=True)
 
+pip_install = [
+    "addfips", "autoflake", "beautifulsoup4", "black",
+    "cachecontrol[filecache]", "cattrs", "charset-normalizer", "dominate",
+    "isort", "matplotlib", "moviepy", "mplcairo", "numpy", "openpyxl",
+    "pandas", "pyarrow", "pybind11", "pycountry", "pyreadr", "requests",
+    "scipy", "us", "xlrd",
+]
+
+if Path("/usr/include/proj_api.h").is_file():
+    pip_install.extend(["cartopy==0.19.0.post1", "--no-binary", "cartopy"])
+else:
+    pip_install.append("cartopy")
+
 check_call(["direnv", "allow", source_dir])
 check_call(["direnv", "exec", source_dir, "pip", "install", "wheel"])
-check_call([
-    "direnv", "exec", source_dir, "pip", "install", "--no-binary", "cartopy",
-    "addfips", "autoflake", "beautifulsoup4", "black",
-    "cachecontrol[filecache]", "cartopy==0.19.0.post1", "cattrs",
-    "charset-normalizer", "dominate", "isort", "matplotlib", "moviepy",
-    "mplcairo", "numpy", "openpyxl", "pandas", "pyarrow", "pybind11",
-    "pycountry", "pyreadr", "requests", "scipy", "us", "xlrd",
-])
+check_call(["direnv", "exec", source_dir, "pip", "install"] + pip_install)
 
 print()
 print("::: Setup complete :::")
