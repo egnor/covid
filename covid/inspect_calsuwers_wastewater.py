0f7e84b662e90e8083c2ddcc6baba575d4a262e7
Dan Egnor <egnor@ofb.net>
2022-08-07 00:19:58 -0700

    start in on wastewater deep dive

--- covid/inspect_calsuwers_wastewater.py
+++ covid/inspect_calsuwers_wastewater.py
@@ -1,5 +1,6 @@
 """Generate specialized plot of Cal-SuWers data"""
 
+import dataclasses
 
 import numpy
 
@@ -9,6 +10,19 @@ import covid.merge_covid_metrics
 import covid.region_data
 
 
+@dataclasses.dataclass(frozen=True, order=True)
+class LabId:
+    id: str
+    name: str
+
+
+@dataclasses.dataclass(frozen=True, order=True)
+class SiteId:
+    id: str
+    name: str
+    pop: int
+
+
 def get_lab_site_metrics(session):
     out = {}
     atlas = covid.build_atlas.get_atlas(session)
@@ -16,29 +30,39 @@ def get_lab_site_metrics(session):
 
     wet_L_div = 3000
     dry_g_div = 3000
+    pmmov_div = 10000000
 
     print("Loading Cal-SuWers wastewater data...")
     df = covid.fetch_calsuwers_wastewater.get_wastewater(session)
     for wwtp, wwtp_rows in df.groupby(level="wwtp_name", sort=False):
         wwtp_rows.reset_index("wwtp_name", drop=True, inplace=True)
-
         wwtp_first = wwtp_rows.iloc[0]
-        site = f"{wwtp} ({wwtp_first['FACILITY NAME']})"
+        site = SiteId(
+            id=wwtp,
+            name=wwtp_first["FACILITY NAME"],
+            pop=wwtp_first.population_served,
+        )
+
         fips = wwtp_first.county_names.split(",")[0].strip()
         fips = int(covid.fetch_calsuwers_wastewater.FIPS_FIX.get(fips, fips))
         region = atlas.by_fips[fips]
 
         covid_key = "COVID positives / day / 100Kp"
-        covid_metrics = region.metrics.covid[covid_key]
+        covid_metric = region.metrics.covid[covid_key]
 
-        for (target, lab), lab_rows in wwtp_rows.groupby(
+        for (target, lab_id), lab_rows in wwtp_rows.groupby(
             level=["pcr_target", "lab_id"]
         ):
             assert target == "sars-cov-2"
-            metrics = out.setdefault(lab, {}).setdefault(site, {})
-            metrics[covid_key] = covid_metrics
+            lab = LabId(
+                id=lab_id,
+                name=covid.fetch_calsuwers_wastewater.LAB_NAMES[lab_id]
+            )
 
-            for (units, gene), rows in lab_rows.groupby(
+            metrics = out.setdefault(lab, {}).setdefault(site, {})
+            metrics[covid_key] = covid_metric
+
+            for (gene, units), rows in lab_rows.groupby(
                 level=["pcr_gene_target", "pcr_target_units"]
             ):
                 samples = rows.pcr_target_avg_conc
@@ -47,10 +71,10 @@ def get_lab_site_metrics(session):
                     units = units[6:]
                 if units.lower() == "copies/l wastewater":
                     samples = samples / wet_L_div
-                    units = f"{units} / {wet_L_div:.0f}"
+                    units = f"{units}/{wet_L_div:.0f}"
                 elif units.lower() == "copies/g dry sludge":
                     samples = samples / dry_g_div
-                    units = f"{units} / {dry_g_div:.0f}"
+                    units = f"{units}/{dry_g_div:.0f}"
 
                 title = f"{target}({gene}) {units}"
                 metrics[title] = covid.region_data.make_metric(
@@ -59,6 +83,45 @@ def get_lab_site_metrics(session):
                     ord=1.0,
                     raw=samples.groupby("sample_collect_date").mean(),
                 )
+
+                flow = rows.flow_rate.groupby("sample_collect_date").mean()
+                metrics["flow L/p/day"] = covid.region_data.make_metric(
+                    c="tab:blue",
+                    em=0,
+                    ord=1.0,
+                    raw=flow * 4.54609e6 / site.pop,
+                )
+
+                tss = rows.tss.groupby("sample_collect_date").mean()
+                metrics["tss mg/L"] = covid.region_data.make_metric(
+                    c="tab:brown",
+                    em=0,
+                    ord=1.0,
+                    raw=tss,
+                )
+
+                hum = rows.hum_frac_mic_conc
+                hum = hum.groupby("sample_collect_date").mean()
+                if hum.count() > 1:
+                    hum_target = rows.iloc[0].hum_frac_target_mic
+                    hum_units = rows.iloc[0].hum_frac_mic_unit
+                    if hum_target.lower() == "pepper mild mottle virus":
+                        hum_target = "PMMoV"
+                        hum = hum / pmmov_div
+                        hum_units = f"{hum_units}/{pmmov_div:.0f}"
+
+                    title = f"{hum_target} {hum_units}"
+                    metrics[title] = covid.region_data.make_metric(
+                        c="tab:purple",
+                        em=0,
+                        ord=1.0,
+                        raw=hum,
+                    )
+
+            for name, metric in list(metrics.items()):
+                if metric.frame.value.count() < 2:
+                    del metrics[name]
+                
 
     return out
 
@@ -75,10 +138,9 @@ if __name__ == "__main__":
 
     lab_site_metrics = get_lab_site_metrics(session)
     for lab, site_metrics in sorted(lab_site_metrics.items()):
-        name = covid.fetch_calsuwers_wastewater.LAB_NAMES[lab]
-        print(f"=== {lab} ({name}) ===")
+        print(f"=== {lab.id} ({lab.name}) ===")
         for site, metrics in sorted(site_metrics.items()):
-            print(f"  {site}")
+            print(f"  {site.id} ({site.name}) {site.pop}p")
             for name, metric in sorted(metrics.items()):
                 print(f"    {metric.debug_line()} {name}")
-        print()
+            print()
